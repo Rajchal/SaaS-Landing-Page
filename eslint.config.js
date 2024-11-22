1df6d477e33a1df262b4d9bea4a722b4595d71bc
Rajchal <rajchalanjal1@gmail.com>
2024-11-22 16:06:38 +0545

    eslint setup and designing

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,9 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-
+import {ReactThreeFiber} from '@react-three/fiber'
+import tailwindcss from 'eslint-plugin-tailwindcss'
+import prettier from 'eslint-config-prettier'
 export default [
   { ignores: ['dist'] },
   {
@@ -22,6 +24,8 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@react-three': ReactThreeFiber,
+      tailwindcss,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -29,10 +33,12 @@ export default [
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
+      "react/no-unknown-property":"off",
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      ...prettier.rules,
     },
   },
 ]
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import React from 'react'
-
+import Header from "./sections/header"
 const App = () => {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>
-        Hello
-      </h1>
-    </div>
+    <main className="overflow-hidden">
+      <Header/>
+    </main>
   )
 }
 
