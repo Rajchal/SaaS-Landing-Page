9a14505f8104c97b7797682429676d173039f2a6
Rajchal <rajchalanjal1@gmail.com>
2024-11-23 19:00:12 +0545

    Add Faq and Features sections; update Header for scroll behavior and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import Faq from "./sections/Faq";
+import Features from "./sections/Features";
 import Header from "./sections/header";
 import Hero from "./sections/Hero";
+import Pricing from "./sections/Pricing";
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
       <Hero />
+      <Features />
+      <Pricing />
+      <Faq />
     </main>
   );
 };
