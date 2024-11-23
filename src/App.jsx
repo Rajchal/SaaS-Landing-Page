f2dd3eb14f8d34801e43b275162fac322c3ce831
Rajchal <rajchalanjal1@gmail.com>
2024-11-23 12:19:07 +0545

    Add Hero section, Button component, and close/magic SVG icons; update Header for mobile navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Header from "./sections/header";
+import Hero from "./sections/Hero";
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
+      <Hero />
     </main>
   );
 };
---
+++ src/components/Button.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Button = ({ icon, children, href, containerClassName }) => {
+  const Inner = () => <>Text</>;
+  return href ? (
+    <a
+      className={clsx(
+        "relative p-0.5 g5 rounded-2xl shadow-500 group",
+        containerClassName
+      )}
+      href={href}
+    >
+      <Inner />
+    </a>
+  ) : (
+    <button
+      className={clsx(
+        "relative p-0.5 g5 rounded-2xl shadow-500 group",
+        containerClassName
+      )}
+      onClick={onClick}
+    >
+      <Inner />
+    </button>
+  );
+};
+
+export default Button;
