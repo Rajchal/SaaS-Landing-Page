a73057463aaee424eae4ee44726c7bb73cf762dc
Rajchal <rajchalanjal1@gmail.com>
2024-11-23 12:36:47 +0545

    Hero section complete

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import clsx from "clsx";
+import { Marker } from "./Marker.jsx";
 
-const Button = ({ icon, children, href, containerClassName }) => {
-  const Inner = () => <>Text</>;
+const Button = ({
+  icon,
+  children,
+  href,
+  containerClassName,
+  onClick,
+  markerFill,
+}) => {
+  const Inner = () => (
+    <>
+      <span className="relative flex items-center min-h-[60px] px-4 g4 rounded-2xl inner-before group-hover:before:opacity-100 overflow-hidden">
+        <span className="absolute -left-[1px]">
+          <Marker markerFill={markerFill} />
+        </span>
+
+        {icon && (
+          <img
+            src={icon}
+            alt="circle"
+            className="size-10 mr-5 object-contain z-10"
+          />
+        )}
+
+        <span className="relative z-2 font-poppins base-bold text-p1 uppercase">
+          {children}
+        </span>
+      </span>
+
+      <span className="glow-before glow-after" />
+    </>
+  );
   return href ? (
     <a
       className={clsx(
@@ -24,5 +54,4 @@ const Button = ({ icon, children, href, containerClassName }) => {
     </button>
   );
 };
-
 export default Button;
