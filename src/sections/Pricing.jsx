e3881ceaca6e48a3e390ff8d3150709f3e9268a3
Rajchal <rajchalanjal1@gmail.com>
2024-11-24 07:26:45 +0545

    Update pricing plans and currency format in Pricing section

--- src/sections/Pricing.jsx
+++ src/sections/Pricing.jsx
@@ -106,7 +106,7 @@ const Pricing = () => {
                         index === 1 ? "text-p3" : "text-p4"
                       )}
                     >
-                      ${" "}
+                      रु{" "}
                       <CountUp
                         start={plan.priceMonthly}
                         end={monthly ? plan.priceMonthly : plan.priceYearly}
