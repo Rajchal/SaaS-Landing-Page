63a4c6c9a23d88abc30ce17212105a955d8f31b7
Rajchal <rajchalanjal1@gmail.com>
2024-11-26 01:51:03 +0545

    Update hero image source in Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
           </div>
           <div className="absolute -top-32 left-[calc(50%-340px)] w-[1230px] pointer-events-none hero-img_res">
             <img
-              src="/images/hero.png"
+              src="/images/herr.png"
               className="size-1230 max-lg:h-auto"
               alt="hero"
             />
